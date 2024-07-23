27f89354505d6e1bf0fe4f592c3c559f92d9a05e
Syllac21 <syllac.online@gmail.com>
2024-07-23 10:39:18 +0200

    page user en cours

--- template/listusers.php
+++ template/listusers.php
@@ -6,8 +6,28 @@ $objUsers = new Users;
 $users = $objUsers->getUsers();
 
 ob_start();
-require_once(__DIR__.'/header.php');
-var_dump($users); ?>
+require_once(__DIR__.'/header.php'); ?>
+
+<table class="mx-auto">
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>prénom</th>
+                        <th>Role</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($users as $user) : ?>
+                    
+                        <tr>
+                            <td><?=$user['firstname'] ?></td>
+                            <td><?=$user['lastname']?> </td>
+                            <td><?=$user['role'] ?></td>
+                        </tr>
+                <?php endforeach;?>
+                </tbody>
+            </table>
 
 <?php $content = ob_get_clean();
 
